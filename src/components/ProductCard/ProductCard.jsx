8b129a05c5a7c6416d9a0a9d1374e9be1a55e68c
Cristian Riffo <cristian.riffo@bairesdev.com>
2021-09-03 19:24:19 +1200

    Using grid for layout
    Co-authored-by: Bruno Moretto <brunogmoretto@hotmail.com>

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -5,6 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
 import Rating from '@material-ui/lab/Rating';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import StarIcon from '@material-ui/icons/Star';
 
@@ -15,22 +16,36 @@ const useStyles = makeStyles({
     media: {
         height: 177,
     },
+    cardArea: {
+        backgroundColor: 'none'
+    },
+    cardActions: {
+        backgroundColor: 'none'
+    }
 });
 
 export const ProductCard = () => {
     const classes = useStyles();
-    return <Card elevation={0} className={classes.root}>
-        <CardActionArea>
+    return <Card component={Grid} container elevation={0} className={classes.root} direction="column" >
+
+        <CardActionArea className={classes.cardArea}>
             <CardMedia className={classes.media} image='https://www.noelleeming.co.nz/shop/render-image/products/183/183120.309.200.jpg' />
             <Typography variant="h6">Endeavour HDMI 2.1 Cable 2m</Typography>
-            <Typography variant="caption">126082</Typography>
-            <Rating name="read-only" value={4} readOnly />
-            <Typography variant="caption">(3)</Typography>
-            <Typography variant="body1">49.00</Typography>
+
+            <Grid container direction="column">
+                <Typography variant="caption">126082</Typography>
+                <Grid container item direction="row" alignItems="center">
+                    <Rating name="read-only" value={4} readOnly />
+                    <Typography variant="caption">(3)</Typography>
+                </Grid>
+                <Typography variant="body1">49.00</Typography>
+            </Grid>
         </CardActionArea>
-        <CardActions>
+        <CardActions className={classes.cardActions}>
+        <Grid container direction="column" alignItems="flex-start">
             <Button variant="contained" color="secondary">Buy</Button>
-            <Button startIcon={<StarIcon />} >Buy </Button>
+            <Button startIcon={<StarIcon />} >add to whishlist </Button>
+            </Grid>
         </CardActions>
     </Card>
 
